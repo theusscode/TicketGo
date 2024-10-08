bd17aec07779734c70ae5adc870a7e334fe8dbde
theusscode <matheusscosta1104@gmail.com>
2024-10-08 18:54:03 -0300

    Primeiro commit: adicionando arquivos do projeto

---
+++ Panel-Admin/scripts/sdmin.js
@@ -0,0 +1,20 @@
+function sortTable(n) {
+    const table = document.getElementById("ticketTable");
+    let rows, switching, i, x, y, shouldSwitch, dir, switchcount = 0;
+    switching = true;
+    dir = "asc";
+    while (switching) {
+        switching = false;
+        rows = table.rows;
+        for (i = 1; i < (rows.length - 1); i++) {
+            shouldSwitch = false;
+            x = rows[i].getElementsByTagName("TD")[n];
+            y = rows[i + 1].getElementsByTagName("TD")[n];
+            if (dir === "asc") {
+                if (x.innerHTML.toLowerCase() > y.innerHTML.toLowerCase()) {
+                    shouldSwitch = true;
+                    break;
+                }
+            } else if (dir === "desc") {
+                if (x.innerHTML.toLowerCase() < y.innerHTML.toLowerCase()) {
+                    shouldSwitch = true;
